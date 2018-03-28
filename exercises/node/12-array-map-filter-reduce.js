e5a331381c06d51609fe68b4317a443e90b85c05
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-03-28 15:38:00 +0100

    12 array map filter reduce

--- exercises/node/12-array-map-filter-reduce.js
+++ exercises/node/12-array-map-filter-reduce.js
@@ -1,12 +1,27 @@
 const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-/** TODO:
+/**
   1. Take the odd numbers from the list,
   2. Double them (i.e. 1 -> 2, 3 -> 6, etc)
   3. Sum them
 */
 
-const result;
+const result = arr
+    .filter(x => {
+      console.log('result called', x);
+      return x % 2 === 1
+    })
+    .map(x => {
+      console.log('map called', x);
+      return x + x;
+    })
+    .reduce((state, x) => {
+      console.log('reduce called', state, x);
+      const newState = state + x;
+      return newState;
+    }, 0);
+
+
 
 console.log(result);
 
