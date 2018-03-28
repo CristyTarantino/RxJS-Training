da521680b2d63d0605667a3d69470318bb1533e4
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-03-28 12:59:21 +0100

    3 unsubscribe

--- exercises/node/3-unsubscribe.js
+++ exercises/node/3-unsubscribe.js
@@ -3,3 +3,29 @@ const data$ = require('./fixtures/1-data.js');
 
 // TODO: Get the subscription and unsubscribe it after 1 second
 
+// Subscribe using callbacks (producer)
+const sub = data$.subscribe(
+    x => console.log(x),
+    err => console.error(err), //if you don't provide err function it is going to throw the error globally
+    () => console.info('done')
+);
+
+// consumer
+setTimeout(() => {
+  sub.unsubscribe();
+}, 1000);
+
+// run node exercises/node/3-unsubscribe.js
+
+// Output
+// 0
+// 1
+// 2
+// 3
+
+/**
+ * It never gets to done because we unsubscribe before complete
+ * a) the producer notifies that it has successfully completed - () => console.info('done')
+ * b) the producer notifies that it has stopped because an error - err => console.error(err),
+ * c) the consumer notifies the producer that no longer cares abut it - sub.unsubscribe();
+ */
