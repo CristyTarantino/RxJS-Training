270604cd2fa6074b21df34af9734031448599ed9
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-03-28 12:38:24 +0100

    exercise 1

--- exercises/node/1-subscribe-with-callbacks.js
+++ exercises/node/1-subscribe-with-callbacks.js
@@ -4,3 +4,24 @@ const data$ = require('./fixtures/1-data.js');
 // TODO: Subscribe to `data$` using three callbacks and log out all values to console.
 // be sure to log errors and completions as well.
 
+// Subscribe using callbacks
+data$.subscribe(
+    x => console.log(x),
+    err => console.error(err),
+    () => console.info('done')
+);
+
+// run node exercises/node/1-subscribe-with-callbacks.js
+
+//Output
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// done
