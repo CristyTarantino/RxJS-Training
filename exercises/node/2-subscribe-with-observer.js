6bb4389fc7d23e03595ef8acfa1e8579d39a670e
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-03-28 12:38:45 +0100

    ex2

--- exercises/node/2-subscribe-with-observer.js
+++ exercises/node/2-subscribe-with-observer.js
@@ -2,3 +2,28 @@
 const data$ = require('./fixtures/1-data.js');
 
 // TODO: create an observer to subscribe to `data$` and log out all values.
+// An observer is an object with at least a next, an error and a complete function
+const observer = {
+  focusOffset() {},
+  next(x) { console.log(x); },
+  error(err) { console.error(err); },
+  complete() {console.info('done'); }
+};
+
+// Subscribe using observer
+data$.subscribe(observer);
+
+// run node exercises/node/2-subscribe-with-observer.js
+
+//Output
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
+// done
