bffd03901665260b0c06c1f0abd980336e16dbc6
Cristina Tarantino <m.cristina.tarantino@gmail.com>
2018-03-28 14:43:45 +0100

    6 observable resource

--- exercises/node/6-observable-ctor-resource.js
+++ exercises/node/6-observable-ctor-resource.js
@@ -1,7 +1,7 @@
 const Rx = require('rxjs/Rx');
 const Resource = require('./fixtures/6-Resource');
 
-// TODO: create an observable with the Observable constructor that
+// create an observable with the Observable constructor that
 // creates a new resource, subscribes to it's "data" event, and tears it
 // down when it's done.
 
@@ -13,7 +13,18 @@ resource.addEventListener('data', handler); // listen for data events
 resource.removeEventListener('data', handler); // stop listening for data events
 */
 
-const source$; // <-- set me!
+const source$ = new Rx.Observable(observer => {
+  const resource = new Resource();
+  const handler = x => observer.next(x);
+
+  resource.addEventLi stener('data', handler);
+
+  return () => {
+    resource.removeEventListener('data', handler);
+  }
+}); // <-- set me!
+
+
 const subscription = source$.subscribe(
   x => console.log(x),
   err => console.error(err),
@@ -23,7 +34,7 @@ const subscription = source$.subscribe(
 setTimeout(() => subscription.unsubscribe(), 2100);
 
 /**
-NOTE: output should be:
+expected output:
 
 Resource: started
 Resource: event listener added
