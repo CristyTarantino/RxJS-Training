b89fd0e97c173ab5176c4e83e4eb44aef0fec364
CristyTarantino <m.cristina.tarantino@gmail.com>
2018-03-29 14:09:25 +0100

    22 composite subscriptions

--- exercises/node/22-composite-subscriptions.js
+++ exercises/node/22-composite-subscriptions.js
@@ -1,6 +1,8 @@
 const { Subscription } = require('rxjs/Subscription');
 const noisyUnsubscriber = require('./fixtures/22-noisy-unsubscriber');
 
+//Imperative way to write subscription
+
 // NOTE: Setup
 const sourceA$ = noisyUnsubscriber('a');
 const sourceB$ = noisyUnsubscriber('b');
@@ -13,14 +15,23 @@ const subC = sourceC$.subscribe(x => console.log(x));
 
 // TODO: manage subscriptions by building a single parent subscription
 
+const sub = new Subscription();
+const childSubA = sub.add(subA);  //this way wraps it into a child Subscription
+const childSubB = sub.add(subB);
+const childSubC = sub.add(subC);
+
 setTimeout(() => {
   // TODO: unsubscribe from `subA` so that it's removed from your
   //       parent subscription
+  // subA.unsubscribe();    // it will leave the child subscription into the parent
+  childSubA.unsubscribe();  // unsubscribe subA but also removes itself from the parent subscription (so it doesn't hold on to a memory reference to the old subscription you don't care about
+                            // this is like doing  --> sub.remove(childSubA); and subA.unsubscribe();
 }, 900);
 
 setTimeout(() => {
-  // TODO: unsubscribe from all remaining subscriptions (`subB` and `subC`)
-  //       using a single parent subscription
+  // unsubscribe from all remaining subscriptions (`subB` and `subC`)
+  // using a single parent subscription
+  sub.unsubscribe();
 }, 1300);
 
 /**
