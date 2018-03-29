60149fa216d1ae6f595f59589657b9d84240634a
CristyTarantino <m.cristina.tarantino@gmail.com>
2018-03-29 13:35:28 +0100

    21 merge strategies

--- exercises/browser/21-merge-strategies/app.js
+++ exercises/browser/21-merge-strategies/app.js
@@ -6,7 +6,6 @@
 
   const { Observable } = Rx;
   const { fromEvent } = Observable;
-  const { mergeMap, scan, map, startWith, concat } = Rx.operators;
 
   const dropClick$ = fromEvent(drop, 'click');
 
@@ -45,7 +44,102 @@
         }
   */
 
+  //DIFFERENT MERGE STRATEGIES
 
+  /*
+    MERGEALL
+    If you have multiple observables A and B
+
+    A            ----a----a--------a----a----|
+    B               ----b--b---b----b-----b----|
+
+
+    subject -----A-----B-------|
+
+    subject.mergeAll()
+            -----*---a*-b-ab---b---ab---a-b----|
+   */
 
   // our stream of drop clicks
+  // dropClick$
+  //   .map(() => addBall(svg))         // play every single one of these and merge these into the output stream
+  //   .mergeAll()                      // takes every observable that arrives from its source and it merges and subscribes their values into the output stream
+  //   .subscribe();
+  // verbose version
+
+  // dropClick$
+  //   .mergeMap(() => addBall(svg))       //or .flatMap(() => addBall(svg))
+  //   .subscribe();
+
+  /*
+    a$.merge(b$, c$); === Observable.merge(a$, b$, c$) === Observable.of(a$, b$, c$).mergeAll();
+   */
+
+  /*
+    CONCATALL waits for each one to complete before it starts the next one
+
+    subject.concatAll()
+            -----*----a----a--------a----a----*----b--b---b----b-----b----|
+   */
+
+  // dropClick$
+  //   .map(() => addBall(svg))         -----| //or concatMap(() => addBall(svg))
+  //   .concatAll()                     -----|
+  //   .subscribe();
+
+  /*
+    SWITCH interrupts the previous request and subscribes to the new one
+
+    subject.switch()
+            -----*----a*--b--b---b----b-----b----|
+
+    used for HTTP requests like autocomplete
+    so you start with searching for fish but then you keep typing because you need fisherman
+   */
+
+    // dropClick$
+    //   .map(() => addBall(svg))       -----| //or switchMap(() => addBall(svg))
+    //   .switch()                      -----|
+    //   .subscribe();
+
+
+  dropClick$
+    // EVENTS COORDINATOR in order to make a stream of actions
+    // .switchMap(() => listenFor('drop a ball'))
+    // .do(x => console.log(x))                        // used for debugging
+    .mergeMap(() => {
+      // return a ball animation observable where
+      return addBall(svg)
+        // when the ball animates, emit an action
+        .mapTo({type: 'BALL_ANIMATE'})
+        // it starts with a BALL_START action
+        .startWith({type: 'BALL_START'})
+        // it ends with a BALL_END action
+        .concat(Rx.Observable.of({type: 'BALL_END'}))
+    })
+    // use a scan for state management (we'll just mutate state, it's okay here) - STATE MANAGEMENT
+    .scan((state, action) => {
+      switch (action.type) {
+        // when you get a BALL_START, increment dropped counter
+        case 'BALL_START':
+          // best practice create a new state -> return {...state, dropped: state.dropped + 1}
+          state.dropped++;
+          break;
+
+        // when you get a BALL_END, increment the completed counter
+        case 'BALL_END':
+          state.completed++;
+          break;
+      }
+
+      return state;
+
+    }, { dropped: 0, completed: 0 })
+    // init scan here
+    // subscribe to our state stream
+    .subscribe(state => {
+      dropped.innerText = state.dropped;
+      completed.innerText = state.completed;
+    });
+
 }());
